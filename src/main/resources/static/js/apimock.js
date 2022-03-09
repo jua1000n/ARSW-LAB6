873d238f354a69d096b471fe80b4180589ee673b
Juan Sebastian Cadavid <juan.cadavid-p@mail.escuelaing.edu.co>
2022-03-08 21:14:16 -0500

    Lab6 front-end logica completo

--- src/main/resources/static/js/apimock.js
+++ src/main/resources/static/js/apimock.js
@@ -4,10 +4,19 @@ apimock=(function(){
 
 	var mockdata=[];
 
-	mockdata["johnconnor"]=	[{author:"johnconnor","points":[{"x":150,"y":120},{"x":215,"y":115}],"name":"house"},
-	 {author:"johnconnor","points":[{"x":340,"y":240},{"x":15,"y":215}],"name":"gear"}];
-	mockdata["maryweyland"]=[{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house2"},
-	 {author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"}];
+	mockdata["johnconnor"]=	[{author:"johnconnor","points":[{"x":150,"y":120}],"name":"house"},
+	 {author:"johnconnor","points":[{"x":340,"y":240}],"name":"gear"},
+		{author:"johnconnor","points":[{"x":340,"y":240}],"name":"mount"}];
+	mockdata["maryweyland"]=[{author:"maryweyland","points":[{"x":140,"y":140}],"name":"house2"},
+	 {author:"maryweyland","points":[{"x":140,"y":140}],"name":"gear2"}];
+	mockdata["bad"]=[{author:"bad","points":[{"x":150,"y":10},{"x":145,"y":112},{"x":15,"y":11}],"name":"blueprint01"},
+	 {author:"bad","points":[{"x":150,"y":10},{"x":145,"y":112},{"x":15,"y":11}],"name":"gear"}];
+	mockdata["arturo"]=[{author:"arturo","points":[{"x":115,"y":115}],"name":"house2"},
+	 {author:"arturo","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"}];
+	mockdata["other"]=[{author:"other","points":[{"x":150,"y":120},{"x":215,"y":115},{"x":115,"y":115}],"name":"house"},
+	 {author:"other","points":[{"x":340,"y":240}],"name":"gear"}];
+	mockdata["carlos"]=[{author:"carlos","points":[{"x":115,"y":115}],"name":"house2"},
+		{author:"carlos","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"}];
 
 
 	return {
@@ -28,6 +37,9 @@ apimock=(function(){
 })();
 
 /*
+
+
+
 Example of use:
 var fun=function(list){
 	console.info(list);
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,15 +1,58 @@
-var Module = (function () {
+app = (function () {
+    let aut;
+    let name;
+    let listAuthor;
 
-    var _privateMethod = function () {
+    const callbackBlue = (listCall) => {
+        const list = listCall.map(blueprint => {
+           return {
+               name: blueprint.name,
+               points: blueprint.points.length
+           }
+        });
+        $("#table-tbody").empty();
+        listAuthor = list;
+        console.log(list);
 
-    };
 
-    var publicMethod = function () {
-        _privateMethod();
-    };
 
-    return {
-        publicMethod: publicMethod
-    };
+
+        list.map(blueprint => {
+            const {name, points} = blueprint;
+            const col = document.createElement('tr');
+
+            col.innerHTML = `
+                <td>${name}</td>
+                <td>${points}</td>
+                `
+            $("#table-tbody").append(col);
+        });
+    }
+
+    const getNameAuthorBlueprint =  () => {
+        aut = document.getElementsByName("author")[0].value;
+        if(aut === "") {
+            alert("No ingreso ningun author")
+        }else {
+            apimock.getBlueprintsByAuthor(aut, callbackBlue);
+        }
+    }
+
+    const getNameAuthorNameBlueprint =  () => {
+        aut = document.getElementsByName("author")[0].value;
+        apimock.getBlueprintsByNameAndAuthor(aut, name, (dibujo) => {
+            
+        });
+    }
+
+    const setNameAuthor = (newName) => {
+        name = newName;
+    }
+
+    return{
+        getNameAuthorBlueprint: getNameAuthorBlueprint,
+        setNameAuthor: setNameAuthor(),
+        getNameAuthorNameBlueprint: getNameAuthorNameBlueprint
+    }
 
 })();
